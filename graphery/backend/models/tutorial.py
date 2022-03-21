e10d0a45d018aaa7cfb247082f9cfce9080479b7
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-03-20 17:26:08 -0700

    django: fix tutorial relation relation fields

--- graphery/backend/models/tutorial.py
+++ graphery/backend/models/tutorial.py
@@ -5,7 +5,7 @@ from . import (
     TimeDateMixin,
     StatusMixin,
     LangMixin,
-    Tag,
+    TagAnchor,
     User,
     unique_with_lang,
 )
@@ -16,12 +16,14 @@ __all__ = ["TutorialAnchor", "Tutorial"]
 class TutorialAnchor(UUIDMixin, TimeDateMixin, StatusMixin, models.Model):
     url = models.CharField("tutorial url", max_length=150, unique=True)
     anchor_name = models.CharField("tutorial anchor name", max_length=200, unique=True)
-    tags = models.ManyToManyField(Tag)
+    tag_anchors = models.ManyToManyField(TagAnchor, related_name="tutorial_anchors")
 
 
 class Tutorial(UUIDMixin, TimeDateMixin, StatusMixin, LangMixin, models.Model):
-    tutorial_anchor = models.ForeignKey(TutorialAnchor, on_delete=models.PROTECT)
-    authors = models.ManyToManyField(User)
+    tutorial_anchor = models.ForeignKey(
+        TutorialAnchor, on_delete=models.PROTECT, related_name="tutorials"
+    )
+    authors = models.ManyToManyField(User, related_name="tutorials")
     title = models.CharField("tutorial title", max_length=300, null=False, blank=False)
     abstract = models.TextField("tutorial abstract", null=False)
     content_markdown = models.TextField("tutorial content in Markdown", null=False)
