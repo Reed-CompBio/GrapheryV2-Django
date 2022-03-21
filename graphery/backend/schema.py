92ccfbf914de788870100a0fdc27368351db18ee
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-03-20 17:06:29 -0700

    graphql: test anchor query in schema

--- graphery/backend/schema.py
+++ graphery/backend/schema.py
@@ -1,16 +1,25 @@
+from typing import List
+
 import strawberry
 from strawberry.django import auth
-from .types import User
+from strawberry.types import Info
+
+from .models import TagAnchor
+from .types import UserType, TagAnchorType
 
 
 @strawberry.type
 class Query:
-    me: User = auth.current_user()
+    me: UserType = auth.current_user()
+
+    @strawberry.field
+    def tag_anchors(self, info: Info) -> List[TagAnchorType]:
+        return TagAnchor.objects.all()
 
 
 @strawberry.type
 class Mutation:
-    login: User = auth.login()
+    login: UserType = auth.login()
     logout = auth.logout()
 
 
