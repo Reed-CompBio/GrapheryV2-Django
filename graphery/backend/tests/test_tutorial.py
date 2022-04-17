78c40a134d00e72fe70e5e1b81ff9ffcf67a3d25
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-04-16 19:25:53 -0700

    test: tutorial bridge test

--- graphery/backend/tests/test_tutorial.py
+++ graphery/backend/tests/test_tutorial.py
@@ -1,21 +1,39 @@
 from __future__ import annotations
 
+from typing import Sequence
+
 import pytest
 
-from .utils import USER_LIST, bridge_test_helper, instance_to_model_info
+from .utils import (
+    USER_LIST,
+    bridge_test_helper,
+    instance_to_model_info,
+    make_request_with_user,
+    FieldChecker,
+)
 from ..baker_recipes import (
     tutorial_anchor_recipe,
     tag_anchor_recipe,
     tutorial_recipe,
     user_recipe,
+    graph_anchor_recipe,
 )
 from ..data_bridge import TutorialAnchorBridge, TutorialBridge, ValidationError
-from ..models import User, UserRoles, Status
+from ..models import (
+    User,
+    UserRoles,
+    Status,
+    GraphOrder,
+    GraphAnchor,
+    OrderedGraphAnchor,
+)
 from ..types import (
     TutorialAnchorMutationType,
     TagAnchorMutationType,
     TutorialMutationType,
     UserMutationType,
+    OrderedGraphAnchorBindingType,
+    GraphAnchorMutationType,
 )
 
 
@@ -27,7 +45,9 @@ def tag_anchors(transactional_db):
 
 @pytest.fixture(scope="function")
 def tutorial_anchor(transactional_db, tag_anchors):
-    tutorial_anchor = tutorial_anchor_recipe.make(tag_anchors=tag_anchors)
+    tutorial_anchor = tutorial_anchor_recipe.make(
+        tag_anchors=tag_anchors, graph_anchors=graph_anchor_recipe.make(_quantity=3)
+    )
     return tutorial_anchor
 
 
@@ -49,16 +69,57 @@ def tutorial(transactional_db, tutorial_anchor, author_users):
     return tutorial
 
 
+@pytest.fixture(scope="function")
+def graph_anchors(transactional_db):
+    graph_anchors = graph_anchor_recipe.make(_quantity=5)
+
+    return graph_anchors
+
+
+class GraphAnchorsChecker(
+    FieldChecker[Sequence[OrderedGraphAnchorBindingType], Sequence[GraphAnchor]]
+):
+    def set_expected_value(self, expected_value) -> FieldChecker:
+        self._expected_value = {
+            ordered_anchor.graph_anchor.id: ordered_anchor
+            for ordered_anchor in expected_value
+        }
+        return self
+
+    def _check(self) -> bool:
+        actual_ordered_anchors: Sequence[
+            OrderedGraphAnchor
+        ] = OrderedGraphAnchor.objects.filter(graph_anchor__in=self._actual_value)
+
+        assert len(actual_ordered_anchors) == len(self._expected_value)
+
+        for actual_ordered_anchor in actual_ordered_anchors:
+            expected_ordered_anchor = self._expected_value.get(
+                actual_ordered_anchor.graph_anchor.id, None
+            )
+            if expected_ordered_anchor is None:
+                return False
+            if expected_ordered_anchor.order != actual_ordered_anchor.order:
+                return False
+
+        return True
+
+
+GRAPH_ANCHOR_CHECKER = GraphAnchorsChecker("graph_anchors")
+
+
 @pytest.mark.parametrize(
     "get_fixture",
     USER_LIST,
     indirect=True,
 )
-def test_tutorial_anchor(rf, tutorial_anchor, tag_anchors, get_fixture: User):
+def test_tutorial_anchor(
+    rf, tutorial_anchor, tag_anchors, get_fixture: User, graph_anchors
+):
     model_infos = [
         TutorialAnchorMutationType(
             id=tutorial_anchor.id,
-            url="new_url",
+            url="new-anchor-url",
             anchor_name="new anchor name",
             item_status=Status.PUBLISHED,
         ),
@@ -71,16 +132,39 @@ def test_tutorial_anchor(rf, tutorial_anchor, tag_anchors, get_fixture: User):
                 TagAnchorMutationType(id=tag_anchor.id)
                 for tag_anchor in tag_anchors[:3]
             ],
+            graph_anchors=[
+                OrderedGraphAnchorBindingType(
+                    graph_anchor=instance_to_model_info(
+                        graph_anchor, GraphAnchorMutationType
+                    ),
+                    order=GraphOrder.HIGH,
+                )
+                for graph_anchor in graph_anchors
+            ],
         ),
     ]
-    old_model_info = instance_to_model_info(tutorial_anchor, TutorialAnchorMutationType)
+
+    old_model_info = instance_to_model_info(
+        tutorial_anchor, TutorialAnchorMutationType, ignore_value="graph_anchors"
+    )
+    old_model_info.graph_anchors = [
+        OrderedGraphAnchorBindingType(
+            graph_anchor=instance_to_model_info(graph_anchor, GraphAnchorMutationType),
+            order=GraphOrder.HIGH,
+        )
+        for graph_anchor in tutorial_anchor.graph_anchors.all()
+    ]
 
     for model_info in model_infos:
-        request = rf.post("/graphql/sync")
-        request.user = get_fixture
+        request = make_request_with_user(rf, get_fixture)
 
         bridge_test_helper(
-            TutorialAnchorBridge, model_info, old_model_info, request, UserRoles.AUTHOR
+            TutorialAnchorBridge,
+            model_info,
+            old_model_info,
+            request,
+            UserRoles.AUTHOR,
+            custom_checker=(GRAPH_ANCHOR_CHECKER,),
         )
 
 
