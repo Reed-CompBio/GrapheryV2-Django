249a295803be76941776edb7f00c529d16507f37
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-06-25 22:40:09 -0700

    graphql: layout apis

--- graphery/backend/schema/__init__.py
+++ graphery/backend/schema/__init__.py
@@ -16,17 +16,19 @@ from .resolvers.queries import (
     resolve_tutorial_anchors,
     resolve_graph_anchors,
     get_tutorial_content,
+    get_graph,
     get_graph_content,
-    get_graph_anchor,
+    get_code,
 )
+from ..models import Code
 from ..types import (
     UserType,
     TagAnchorType,
     TutorialType,
     TutorialAnchorType,
     GraphAnchorType,
-    GraphDescriptionType,
     GraphType,
+    GraphDescriptionType,
 )
 
 from ..types.filters import (
@@ -39,27 +41,32 @@ __all__ = ["schema"]
 @strawberry.type
 class Query:
     me: Optional[UserType] = strawberry.field(resolver=resolve_current_user)
-    tag_anchors: List[Optional[TagAnchorType]] = strawberry_django.field(
-        filters=TagAnchorFilter
-    )
-    tutorial_anchors: List[Optional[TutorialAnchorType]] = strawberry_django.field(
+    tag_anchors: List[TagAnchorType] = strawberry_django.field(filters=TagAnchorFilter)
+    tutorial_anchors: List[TutorialAnchorType] = strawberry_django.field(
         resolve_tutorial_anchors
     )
-    graph_anchors: List[Optional[GraphAnchorType]] = strawberry_django.field(
-        resolve_graph_anchors,
+    graph_anchors: List[GraphAnchorType] = strawberry_django.field(
+        resolve_graph_anchors
     )
     tutorial_content: Optional[TutorialType] = strawberry_django.field(
-        get_tutorial_content,
+        get_tutorial_content
     )
-    graph_content: List[Optional[GraphDescriptionType]] = strawberry_django.field(
-        get_graph_content,
+    graph_content: Optional[GraphDescriptionType] = strawberry_django.field(
+        get_graph_content
     )
-    graph: Optional[GraphType] = strawberry_django.field(get_graph_anchor)
+    graph: Optional[GraphType] = strawberry_django.field(get_graph)
+    code: Optional[Code] = strawberry_django.field(get_code)
 
 
 @strawberry.type
 class Mutation:
     login: Optional[UserType] = auth.login()
+    register: Optional[UserType] = strawberry.mutation(
+        resolver=resolve_user_register_mutation
+    )
+    mutate_tag: Optional[TagAnchorType] = strawberry.mutation(
+        resolver=resolve_tag_mutation
+    )
     logout: NoneType = auth.logout()
 
 
--- graphery/backend/schema/resolvers/queries/__init__.py
+++ graphery/backend/schema/resolvers/queries/__init__.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
-from time import sleep
-
-import strawberry
 from uuid import UUID
 
-from typing import List, Optional, Sequence
+from typing import List, Optional
 
 from strawberry.types import Info
 
-from ....models import TutorialAnchor, GraphAnchor, Tutorial, LangCode, Graph
+from ....models import (
+    TutorialAnchor,
+    GraphAnchor,
+    Tutorial,
+    LangCode,
+    Graph,
+    GraphDescription,
+    Code,
+)
 from ....types import (
     UserType,
     TutorialAnchorType,
@@ -19,14 +24,17 @@ from ....types import (
     GraphAnchorFilter,
     GraphDescriptionType,
     GraphType,
+    CodeType,
 )
 
 __all__ = [
     "resolve_current_user",
     "resolve_tutorial_anchors",
-    "get_tutorial_content",
-    "get_graph_anchor",
     "resolve_graph_anchors",
+    "get_tutorial_content",
+    "get_graph_content",
+    "get_graph",
+    "get_code",
 ]
 
 
@@ -58,8 +66,28 @@ def get_tutorial_content(
     return query_set[0] if query_set else None
 
 
-def get_graph_anchor(
+def get_graph_content(
     info: Info,
-    ident: Optional[UUID],
+    url: Optional[str],
+    anchor_id: Optional[UUID],
+    lang: LangCode = LangCode.EN,
+) -> Optional[GraphDescriptionType]:
+    if url:
+        return GraphDescription.objects.filter(url=url, lang_code=lang)
+    elif anchor_id:
+        return GraphDescription.objects.filter(
+            graph_anchor__id=anchor_id, lang_code=lang
+        )
+    else:
+        return None
+
+
+def get_graph(
+    info: Info,
+    anchor_id: UUID,
 ) -> Optional[GraphType]:
-    return Graph.objects.get(graph_anchor__id=ident)
+    return Graph.objects.get(graph_anchor__id=anchor_id)
+
+
+def get_code(info: Info, code_id: UUID) -> Optional[CodeType]:
+    return Code.objects.get(id=code_id)
